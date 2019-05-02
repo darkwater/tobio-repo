1f3ab26d692484ef3155f3dc43716fa77eb4c7ba
Sam Lakerveld <darkwater124@gmail.com>
2019-05-03 01:54:00 +0200

    Behave more like a file browser

--- src/main.rs
+++ src/main.rs
@@ -1,138 +1,139 @@
-#![allow(proc_macro_derive_resolution_fallback)] // oh my god DIESEL
 #![feature(proc_macro_hygiene, decl_macro)]
 
-#[macro_use] extern crate diesel;
-
-use diesel::{prelude::*, SqliteConnection};
 use itertools::Itertools;
 use rocket::fairing::AdHoc;
 use rocket::http::{hyper::header, Method, Status};
 use rocket::{self, get, routes};
-use rocket_contrib::{database, json::Json};
+use rocket_contrib::json::Json;
 use serde::Serializer;
 use serde_derive::Serialize;
 use serde_json::value::RawValue;
 use std::error::Error as StdError;
-use std::path::{Path, PathBuf};
-
-mod schema;
+use std::ffi::OsString;
+use std::fs::DirEntry;
+use std::fs;
+use std::path::{Component, Path, PathBuf};
 
 type StdResult<T> = Result<T, Box<dyn StdError>>;
 
-#[database("store")]
-struct DbConn(SqliteConnection);
+const VIDEO_ROOT: &'static str = "/home/dark/tobio";
 
 #[derive(Serialize)]
-struct BrowseVideoResponse {
-    entry: Option<VideoEntry>,
-    crumbs: Vec<String>,
-    children: Vec<VideoEntry>,
+struct BrowseResponse {
+    crumbs:   Vec<Crumb>,
+    children: Vec<MediaEntry>,
 }
 
-#[derive(Serialize, Queryable)]
-struct VideoEntry {
-    #[serde(skip)]
-    id:             i32,
-    #[serde(skip)]
-    parent:         i32,
-    #[serde(rename = "entryType")]
-    entry_type:     String,
-    key:            String,
-    label:          String,
-    url:            Option<String>,
-    #[serde(serialize_with = "json_raw")]
-    extra:          Option<String>,
-    #[serde(skip)]
-    provider_extra: Option<String>,
+#[derive(Serialize)]
+struct Crumb {
+    path:  String,
+    label: String,
 }
 
-fn json_raw<S: Serializer>(opt: &Option<String>, ser: S) -> Result<S::Ok, S::Error> {
-    ::serde::Serialize::serialize(&opt.as_ref().map(|s| RawValue::from_string(s.clone()).expect("invalid extra json")), ser)
-}
+impl Crumb {
+    fn from_path<P: AsRef<Path>>(path: P) -> Vec<Crumb> {
+        let mut crumbs = path.as_ref()
+            .ancestors()
+            .map(|p| {
+                let mut path = PathBuf::from("/");
+                path.push(p);
 
-impl VideoEntry {
-    pub fn by_parent_key(conn: &SqliteConnection, parent: i32, key: &str) -> StdResult<Option<VideoEntry>> {
-        use crate::schema::entries::dsl;
+                dbg!(&path);
 
-        Ok(dsl::entries
-            .filter(dsl::parent.eq(parent))
-            .filter(dsl::key.eq(key))
-            .first(conn)
-            .optional()?)
-    }
+                Crumb {
+                    path:  path.to_string_lossy().to_string(),
+                    label: p.file_name().map(|s| s.to_str().expect("utf8 filename")).unwrap_or("/").to_string(),
+                }
+            })
+            .collect::<Vec<_>>();
 
-    pub fn by_path(conn: &SqliteConnection, path: &Path) -> StdResult<Option<(VideoEntry, Vec<String>)>> {
-        let mut parent_id = 0;
-        let mut crumbs = vec![];
-        let mut entry = None;
-
-        for seg in path.iter() {
-            entry = VideoEntry::by_parent_key(conn, parent_id, &seg.to_string_lossy())?;
-
-            match &entry {
-                Some(e) => {
-                    crumbs.push(e.label.clone());
-                    parent_id = e.id;
-                },
-                None => return Ok(None),
-            }
-        }
-
-        Ok(Some((entry.unwrap(), crumbs)))
-    }
-
-    pub fn children_by_parent(conn: &SqliteConnection, parent: i32) -> StdResult<Vec<VideoEntry>> {
-        use crate::schema::entries::dsl;
-
-        Ok(dsl::entries
-           .filter(dsl::parent.eq(parent))
-           .load(conn)?)
-    }
-
-    pub fn children(&self, conn: &SqliteConnection) -> StdResult<Vec<VideoEntry>> {
-        VideoEntry::children_by_parent(conn, self.id)
+        crumbs.reverse();
+        crumbs
     }
 }
 
-#[get("/video")]
-fn video_root(db: DbConn) -> StdResult<Json<BrowseVideoResponse>> {
-    let children = VideoEntry::children_by_parent(&db, 0)?;
+#[derive(Serialize)]
+struct MediaEntry {
+    folder:   bool,
+    path:     String,
+    filename: String,
+    // #[serde(serialize_with = "json_raw")]
+    // extra:          Option<String>,
+    // #[serde(skip)]
+    // provider_extra: Option<String>,
+}
 
-    let res = BrowseVideoResponse {
-        entry: None,
-        crumbs: vec![],
+impl MediaEntry {
+    fn from(entry: DirEntry) -> StdResult<MediaEntry> {
+        let path = entry.path().canonicalize()?
+            .strip_prefix(VIDEO_ROOT).unwrap()
+            .to_str()
+            .expect("utf8 filename")
+            .to_string();
+
+        let filename = entry.file_name()
+            .into_string()
+            .expect("utf8 filename");
+
+        let folder = entry.file_type()?.is_dir();
+
+        Ok(MediaEntry {
+            folder,
+            path,
+            filename,
+        })
+    }
+}
+
+// fn json_raw<S: Serializer>(opt: &Option<String>, ser: S) -> Result<S::Ok, S::Error> {
+//     ::serde::Serialize::serialize(&opt.as_ref().map(|s| RawValue::from_string(s.clone()).expect("invalid extra json")), ser)
+// }
+
+impl MediaEntry {
+    fn list_dir<P: AsRef<Path>>(path: P) -> StdResult<Vec<MediaEntry>> {
+        let path = path.as_ref().strip_prefix("/").unwrap_or(path.as_ref());
+        let path = PathBuf::from(VIDEO_ROOT).join(path);
+
+        path.read_dir()?
+            .filter_map(|e| e.ok())
+            .map(|entry| MediaEntry::from(entry))
+            .collect::<Result<Vec<_>, _>>()
+    }
+}
+
+#[get("/browse")]
+fn browse_root() -> StdResult<Json<BrowseResponse>> {
+    let crumbs   = Crumb::from_path("/");
+    let children = MediaEntry::list_dir("/")?;
+
+    let res = BrowseResponse {
+        crumbs,
         children,
     };
 
     Ok(Json(res))
 }
 
-#[get("/video/<path..>")]
-fn video_browse(path: PathBuf, db: DbConn) -> StdResult<Option<Json<BrowseVideoResponse>>> {
-    let entry = VideoEntry::by_path(&db, &path)?;
-    if entry.is_none() {
-        return Ok(None);
-    }
+#[get("/browse/<path..>")]
+fn browse_path(path: PathBuf) -> StdResult<Json<BrowseResponse>> {
+    let crumbs   = Crumb::from_path(&path);
+    let children = MediaEntry::list_dir(&path)?;
 
-    let (entry, crumbs) = entry.unwrap();
-    let children        = entry.children(&db)?;
-    let entry           = Some(entry);
-
-    let res = BrowseVideoResponse {
-        entry, crumbs, children,
+    let res = BrowseResponse {
+        crumbs,
+        children,
     };
 
-    Ok(Some(Json(res)))
+    Ok(Json(res))
 }
 
 fn main() {
     rocket::ignite()
         .mount("/", routes![
-               video_root, video_browse,
+               browse_root, browse_path,
         ])
-        .attach(DbConn::fairing())
         .attach(AdHoc::on_response("Set headers", |req, res| {
-            res.set_header(header::AccessControlAllowOrigin::Value("http://localhost:4200".to_owned()));
+            res.set_header(header::AccessControlAllowOrigin::Value("http://172.24.0.3:4200".to_owned()));
 
             if req.method() == Method::Options && res.status() == Status::NotFound {
                 res.set_status(Status::Ok);
